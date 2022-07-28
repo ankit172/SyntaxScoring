94e761027edded7411dcb6df44ecf6e0669d2c73
Ankit Bhargava <ankit.bhargava@aptiv.com>
2022-07-28 16:30:10 +0200

    Read input text file into stream object
    - Read input text files into InputStream buffer first and then organise
      lines into stream elements.
    - Each lines will be used for logic to identify the incorrect character.
    - Print line so that input file reading can be verified.

--- app/src/main/java/com/example/syntaxscoring/MainActivity.java
+++ app/src/main/java/com/example/syntaxscoring/MainActivity.java
@@ -2,7 +2,14 @@ package com.example.syntaxscoring;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Stream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Access text view where total syntax error score can be displayed
+        TextView resultTextView = (TextView) findViewById(R.id.result);
+
+        // Read the input resource file into input stream, which contain the sequence of data
+        // arranged in line, meaning each line has return carriage
+        Resources res = getResources();
+        InputStream inputStream = res.openRawResource(R.raw.input1);
+
+        // Organise identified lines in stream elements
+        Stream<String> lines = new BufferedReader(new InputStreamReader(inputStream)).lines();
+
+        // Read per line from the stream
+        lines.forEach(line -> System.out.println(line));
     }
 }
